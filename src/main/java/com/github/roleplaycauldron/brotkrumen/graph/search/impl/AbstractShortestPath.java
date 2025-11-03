f48ef405d1fa7917be407d703eb81d10e44a9150
Lorias-Jak <jannik.k11@gmail.com>
2025-11-03 10:04:28 +0100

    added Velyn-N to the author list

--- src/main/java/com/github/roleplaycauldron/brotkrumen/graph/search/impl/AbstractShortestPath.java
+++ src/main/java/com/github/roleplaycauldron/brotkrumen/graph/search/impl/AbstractShortestPath.java
@@ -23,7 +23,7 @@ import java.util.function.Predicate;
 abstract class AbstractShortestPath implements PathAlgorithm {
 
     /**
-     * Constructor.
+     * The default constructor.
      */
     public AbstractShortestPath() {
         // Empty
